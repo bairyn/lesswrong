13d91b31609e87baa9a5d9adf4ddb414aface014
bairyn <ByronJohnsonFP@gmail.com>
2013-07-22 20:32:14 +0000

    Model user registration and logins in base_page.

--- gless/lib/pages/lesswrong_base_page.rb
+++ gless/lib/pages/lesswrong_base_page.rb
@@ -1,5 +1,84 @@
 module LessWrong
   class BasePage < Gless::BasePage
-    # TODO: more
+    # user_div iff !login_div
+    element :user_div,  :div, id: 'side-user', child: :logout, validator: false  #FIXME: duplicate ids when viewing other user logged in; workaround by restricting by child.
+    element :login_div, :div, id: 'side-login', validator: false
+
+    # Logged in
+    userlink_regexp = %r{^/user/(.*)}
+    element :userlink,      :link, href: userlink_regexp, parent: :user_div, validator: false
+    element :logout_button, :link, href: %r{/logout/?$}, click_destination: HomePage, validator: false
+
+    # Not logged in
+    element :username,        :text_field, id: 'username', validator: false
+    element :password,        :text_field, id: 'password', validator: false
+    element :login_button,    :button,     type: 'submit', text: 'Login', parent: :login_div, validator: false
+    element :register_link,   :link,       text: 'Register', validator: false
+    element :user_reg,        :text_field, id: 'user_reg', validator: false, cache: false
+    element :password_reg,    :text_field, id: 'passwd_reg', validator: false, cache: false
+    element :password2_reg,   :text_field, id: 'passwd2_reg', validator: false, cache: false
+    element :register_button, :button,     text: 'Create account', validator: false, cache: false
+    element :reg_popup,       :div,        id: 'loginpopup', validator: false, cache: false
+    element :reg_close ,      :link,       text: 'Close this window', validator: false, cache: false
+
+    # @return [String, NilClass] The username if logged in; nil otherwise.
+    def logged_in_as
+      (login_div.exists?) ? nil : userlink.text[userlink_regexp]
+    end
+
+    # @return [Boolean] Whether the user is currently logged in.  Approximate
+    #   alias of +logged_in_as+.
+    def logged_in?
+      !!logged_in_as
+    end
+
+    # Log out.  If +check+, ensure that the user is logged out by checking
+    # whether the user is already logged out.
+    def logout check = true
+      logout_button.click unless check && !logged_in?
+    end
+
+    # Log in using the given credentials, logging out as necessary unless
+    # the +check+ argument is false.
+    def login username, password, check = true
+      if !check || logged_in_as != username
+        logout if check && (logged_in?) && logged_in_as != username
+        self.username.set username
+        self.password.set password
+        login_button.click
+      end
+    end
+
+    # Register, setting the password to the username, skipping if already
+    # signed in to the username provided.  If registration fails, emit a
+    # warning if +warn_already_registered+ is true and attempt to log in instead.
+    def register username, warn_already_registered = true
+      return if logged_in_as == username
+      logout true
+
+      register_link.click
+      user_reg.set username
+      password_reg.set username
+      password2_reg.set username
+      register_button.click
+
+      if reg_popup_open?
+        @session.log.warn "register: registration failed.  Trying to log in before failing..." if warn_already_registered
+
+        reg_close.click
+
+        login username, username, true
+
+        if reg_popup_open?
+          raise 'register: Could not register user or log in'
+        else
+          @session.log.warn "register: Successfuly logged in." if warn_already_registered
+        end
+      end
+    end
+
+    def reg_popup_open?
+      reg_popup.exists? and reg_popup.style !~ /\bdisplay:\s*none(;|\b)/
+    end
   end
 end
